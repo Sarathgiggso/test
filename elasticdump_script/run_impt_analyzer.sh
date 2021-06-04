d3349736415bbc4b3e844819319a4c68630b9bda
Ubuntu <ubuntu@giggso-master.cluster.local>
2021-06-04 18:55:15 +0000

    elastic dumps updated

--- elasticdump_script/run_impt_analyzer.sh
+++ elasticdump_script/run_impt_analyzer.sh
@@ -2,7 +2,7 @@
 var=$(kubectl get nodes -o wide  | grep "elastic")
 set -- $var
 path= pwd
-./import_analyzer.sh http://elastic:changeme@$6:30920 $path/dump/analyzer
+./import_analyzer.sh http://elastic:changeme@$6:30920 /home/ubuntu/ggservices-deployment/elasticdump_script/dump/analyzer
 
 
 #./import_analyzer.sh http://elastic:changeme@$1:9200 /home/ubuntu/ggazure-int/giggso_service_deployment/giggso-kubernetes-manifest/elasticdump_script/dump/analyzer
--- elasticdump_script/run_impt_data.sh
+++ elasticdump_script/run_impt_data.sh
@@ -2,6 +2,6 @@
 var=$(kubectl get nodes -o wide  | grep "elastic")
 set -- $var
 path= pwd
-./import_data.sh http://elastic:changeme@$6:30920 $path/dump/data
+./import_data.sh http://elastic:changeme@$6:30920 /home/ubuntu/ggservices-deployment/elasticdump_script/dump/data
 
 #./import_data.sh  http://elastic:changeme@$1:9200  /home/ubuntu/dump_data_temp/
