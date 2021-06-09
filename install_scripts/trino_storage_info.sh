c77970491ac4d8b814e94cd4dd7d212d24dc6c5d
Ubuntu <ubuntu@giggso-master.cluster.local>
2021-06-09 06:43:17 +0000

    sed command bug fixed

--- install_scripts/trino_storage_info.sh
+++ install_scripts/trino_storage_info.sh
@@ -1,9 +1,10 @@
 #This script will insert the trino storage name and secret in the necessary files`
-sed -i 's/storageaccountname/$1/g' ../trino/metastore-cfg.yaml
-sed -i 's/storageaccesskey/$2/g' ../trino/metastore-cfg.yaml
-sed -i 's/storageaccountname/$1/g' ../trino/trino-cfgs.yaml
-sed -i 's/storageaccesskey/$2/g' ../trino/trino-cfgs.yaml
-sed -i 's/storageaccountname/$1/g' ../AzUpload/azupload-config.yaml
-sed -i 's/storageaccesskey/$2/g' ../AzUpload/azupload-config.yaml
-sed -i 's/storageaccountname/$1/g' ../trino/schema_creation.sh
-sed -i 's/storageaccesskey/$2/g' ../trino/schema_creation.sh
+sed -i "s|storageaccountname|$1|g" ../trino/metastore-cfg.yaml
+sed -i "s|storageaccesskey|$2|g" ../trino/metastore-cfg.yaml
+sed -i "s|storageaccountname|$1|g" ../trino/trino-cfgs.yaml
+sed -i "s|storageaccesskey|$2|g" ../trino/trino-cfgs.yaml
+sed -i "s|storageaccountname|$1|g" ../AzUpload/azupload-config.yaml
+sed -i "s|storageaccesskey|$2|g" ../AzUpload/azupload-config.yaml
+sed -i "s|storageaccountname|$1|g" ../trino/schema_creation.sh
+sed -i "s|storageaccesskey|$2|g" ../trino/schema_creation.sh
+
