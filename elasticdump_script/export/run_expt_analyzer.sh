99cc25e22580e5d674da521ae40617f9115c2236
Ubuntu <ubuntu@giggso-master.cluster.local>
2021-06-11 14:22:13 +0000

    elastic export scripts removed

--- elasticdump_script/export/run_expt_analyzer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-var=$(kubectl get nodes -o wide  | grep "elastic")
-set -- $var
-path= pwd
-./export_analyzer.sh http://elastic:changeme@$6:30920 /home/ubuntu/ggservices-deployment/elasticdump_script/export/dump/analyzer
-
