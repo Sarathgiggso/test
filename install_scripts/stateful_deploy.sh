8b48a37c3f2cde1738e86506b6ac3c80b0705736
Sarath M <91065059+Sarathgiggso@users.noreply.github.com>
2021-10-12 13:16:40 +0530

    Create stateful_deploy.sh

---
+++ install_scripts/stateful_deploy.sh
@@ -0,0 +1,37 @@
+
+echo Installing helm
+./get_helm.sh --version v3.2.4
+
+cd ..;
+
+#To configure docker hub password to pull images from private repo
+echo creating docker hub credentials as secret
+kubectl apply -f ./docker-secret.yaml
+
+# to create storage class
+echo Creating storageclass for PVCs
+kubectl apply -f ./storage_class/ 
+
+# to create NFS Provisioner for wildfly
+echo Creating nfs provisioner for web storage
+helm upgrade -i nfs-giggso nfs-server -f ./nfs-server/values.yaml
+
+#to deploy kafka
+helm upgrade -i zookeeper-cluster zookeeper -f ./zookeeper/values.yaml 
+helm upgrade -i kafka-cluster kafka -f ./kafka/values.yaml 
+
+#to deploy ksqldb
+helm upgrade -i gg-ksqldb ksqldb -f ./ksqldb/values.yaml
+kubectl apply -f ksql-client/ksql-cli.yaml
+
+#to deploy trino
+kubectl apply -f ./trino/
+
+# to deploy kibana service
+kubectl apply -f ./kibana/
+
+# to deploy ruleengine service
+kubectl apply -f ./ruleengine/
+
+# to deploy ruleengine service
+kubectl apply -f ./ruleengine/
