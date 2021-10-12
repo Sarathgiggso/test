af25c81eee80616ce04649b374614a66d44d0778
Sarath M <91065059+Sarathgiggso@users.noreply.github.com>
2021-10-12 14:29:44 +0530

    Update onetime_deploy.sh

--- install_scripts/onetime_deploy.sh
+++ install_scripts/onetime_deploy.sh
@@ -20,30 +20,7 @@ helm upgrade -i nfs-giggso nfs-server -f ./nfs-server/values.yaml
 echo Installing mariadb cluster
 
 helm upgrade -i giggso-mariadb mariadb-cluster -f ./mariadb-cluster/values-production.yaml
-#(cd ./docker-mariadb;
-
-#docker-compose up -d)
-
-# to create elasticsearch cluster
-echo Installing Elastic cluster ET:5-10mins
- # To create data node group
-helm upgrade -i es-giggsodata elasticsearch -f ./elasticsearch/giggsodata.yml
-sleep 2m
-# To create master nodegroup
-helm upgrade -i es-master elasticsearch -f ./elasticsearch/master.yml
-#kubectl apply -f elasticsearch/rollover-deployment.yaml
- # To create neo4j node
-#helm upgrade -i neo4j neo4j-community -f ./neo4j-community/values.yaml
-echo Installing Neo4j
-kubectl create namespace nginx-ingress
-helm upgrade -i neo4j neo4j-community -f ./neo4j-community/values.yaml --namespace nginx-ingress
-
-# To install monitoring service
-echo Installing monitoring services
-helm repo add prometheus-community    https://prometheus-community.github.io/helm-charts
-kubectl create namespace monitoring
-helm install --namespace monitoring kops-cluster-monitoring prometheus-community/kube-prometheus-stack  -f ./kube-prometheus-stack/values.yaml --version 13.10.0
-
+#(cd ./docker-mariadb)
 sleep 5m
 
 #to deploy kafka
